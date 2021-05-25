740067d60b216133e25b3597be99af940e94b876
JaewonMin <mjw8523@gmail.com>
2021-05-25 20:13:25 +0900

    clean baseline code

--- code/dkt/trainer.py
+++ code/dkt/trainer.py
@@ -1,7 +1,7 @@
 import os
 import torch
 import numpy as np
-
+import wandb
 
 from .dataloader import get_loaders
 from .optimizer import get_optimizer
@@ -10,7 +10,6 @@ from .criterion import get_criterion
 from .metric import get_metric
 from .model import LSTM, LSTMATTN, Bert, LastQueryTransformer
 
-import wandb
 
 def run(args, train_data, valid_data):
     train_loader, valid_loader = get_loaders(args, train_data, valid_data)
@@ -23,10 +22,15 @@ def run(args, train_data, valid_data):
     optimizer = get_optimizer(model, args)
     scheduler = get_scheduler(optimizer, args)
 
+    # args에 wandb_name을 설정해주었을때만 wandb로 저장하도록 설정하였습니다.
+    if args.wandb_name:
+        wandb.init(project='project name', entity='ekzm8523', config=vars(args))
+        wandb.run.name = args.wandb_name
+        wandb.watch(model)
+
     best_auc = -1
     early_stopping_counter = 0
     for epoch in range(args.n_epochs):
-
         print(f"Start Training: Epoch {epoch + 1}")
         
         ### TRAIN
@@ -36,17 +40,25 @@ def run(args, train_data, valid_data):
         auc, acc,_ , _ = validate(valid_loader, model, args)
 
         ### TODO: model save or early stopping
-        wandb.log({"epoch": epoch, "train_loss": train_loss, "train_auc": train_auc, "train_acc":train_acc,
-                  "valid_auc":auc, "valid_acc":acc})
+        if args.wandb_name:
+            wandb.log({"epoch": epoch,
+                       "train_loss": train_loss,
+                       "train_auc": train_auc,
+                       "train_acc": train_acc,
+                       "valid_auc": auc,
+                       "valid_acc": acc})
+        
         if auc > best_auc:
             best_auc = auc
             # torch.nn.DataParallel로 감싸진 경우 원래의 model을 가져옵니다.
             model_to_save = model.module if hasattr(model, 'module') else model
-            save_checkpoint({
-                'epoch': epoch + 1,
-                'state_dict': model_to_save.state_dict(),
+            save_checkpoint(
+                state={
+                    'epoch': epoch + 1,
+                    'state_dict': model_to_save.state_dict(),
                 },
-                args.model_dir, 'model.pt',
+                model_dir=args.model_dir,
+                model_filename='model.pt'
             )
             early_stopping_counter = 0
         else:
@@ -69,10 +81,10 @@ def train(train_loader, model, optimizer, args):
     total_targets = []
     losses = []
     for step, batch in enumerate(train_loader):
-        input = process_batch(batch, args)
-        preds = model(input)
-        targets = input[3] # correct
-
+        # items -> test, question, tag, correct, mask, interaction, gather_index
+        items = process_batch(batch, args)
+        preds = model(items)
+        targets = items[3]  # correct
 
         loss = compute_loss(preds, targets)
         update_params(loss, model, optimizer, args)
@@ -81,20 +93,15 @@ def train(train_loader, model, optimizer, args):
             print(f"Training steps: {step} Loss: {str(loss.item())}")
         
         # predictions
-        preds = preds[:,-1]
-        targets = targets[:,-1]
+        preds = preds[:, -1]
+        targets = targets[:, -1]
 
-        if args.device == 'cuda':
-            preds = preds.to('cpu').detach().numpy()
-            targets = targets.to('cpu').detach().numpy()
-        else: # cpu
-            preds = preds.detach().numpy()
-            targets = targets.detach().numpy()
+        preds = preds.to('cpu').detach().numpy()
+        targets = targets.to('cpu').detach().numpy()
         
         total_preds.append(preds)
         total_targets.append(targets)
         losses.append(loss)
-      
 
     total_preds = np.concatenate(total_preds)
     total_targets = np.concatenate(total_targets)
@@ -103,6 +110,7 @@ def train(train_loader, model, optimizer, args):
     auc, acc = get_metric(total_targets, total_preds)
     loss_avg = sum(losses)/len(losses)
     print(f'TRAIN AUC : {auc} ACC : {acc}')
+
     return auc, acc, loss_avg
     
 
@@ -112,22 +120,17 @@ def validate(valid_loader, model, args):
     total_preds = []
     total_targets = []
     for step, batch in enumerate(valid_loader):
-        input = process_batch(batch, args)
-
-        preds = model(input)
-        targets = input[3] # correct
-
+        # items -> test, question, tag, correct, mask, interaction, gather_index
+        items = process_batch(batch, args)
+        preds = model(items)
+        targets = items[3]  # correct
 
         # predictions
-        preds = preds[:,-1]
-        targets = targets[:,-1]
+        preds = preds[:, -1]
+        targets = targets[:, -1]
     
-        if args.device == 'cuda':
-            preds = preds.to('cpu').detach().numpy()
-            targets = targets.to('cpu').detach().numpy()
-        else: # cpu
-            preds = preds.detach().numpy()
-            targets = targets.detach().numpy()
+        preds = preds.to('cpu').detach().numpy()
+        targets = targets.to('cpu').detach().numpy()
 
         total_preds.append(preds)
         total_targets.append(targets)
@@ -137,11 +140,9 @@ def validate(valid_loader, model, args):
 
     # Train AUC / ACC
     auc, acc = get_metric(total_targets, total_preds)
-    
     print(f'VALID AUC : {auc} ACC : {acc}\n')
 
     return auc, acc, total_preds, total_targets
-
 
 
 def inference(args, test_data):
@@ -150,57 +151,49 @@ def inference(args, test_data):
     model.eval()
     _, test_loader = get_loaders(args, None, test_data)
     
-    
     total_preds = []
-    
     for step, batch in enumerate(test_loader):
-        input = process_batch(batch, args)
-
-        preds = model(input)
+        # items -> test, question, tag, correct, mask, interaction, gather_index
+        items = process_batch(batch, args)
+        preds = model(items)
         
 
         # predictions
-        preds = preds[:,-1]
-        
-
-        if args.device == 'cuda':
-            preds = preds.to('cpu').detach().numpy()
-        else: # cpu
-            preds = preds.detach().numpy()
+        preds = preds[:, -1]
+        preds = preds.to('cpu').detach().numpy()
             
         total_preds+=list(preds)
 
     write_path = os.path.join(args.output_dir, "output.csv")
+
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)    
+
     with open(write_path, 'w', encoding='utf8') as w:
         print("writing prediction : {}".format(write_path))
         w.write("id,prediction\n")
-        for id, p in enumerate(total_preds):
-            w.write('{},{}\n'.format(id,p))
-
-
+        for id, pred in enumerate(total_preds):
+            w.write('{},{}\n'.format(id, pred))
 
 
 def get_model(args):
     """
     Load model and move tensors to a given devices.
     """
-    if args.model == 'lstm': model = LSTM(args)
-    if args.model == 'lstmattn': model = LSTMATTN(args)
-    if args.model == 'bert': model = Bert(args)
-    if args.model == 'lastquery': model = LastQueryTransformer(args)
-
-    model.to(args.device)
-
-    return model
+    if args.model == 'lstm':
+        model = LSTM(args)
+    if args.model == 'lstmattn':
+        model = LSTMATTN(args)
+    if args.model == 'bert':
+        model = Bert(args)
+    if args.model == 'lastquery':
+        model = LastQueryTransformer(args)
+    return model.to(args.device)
 
 
 # 배치 전처리
 def process_batch(batch, args):
-
     test, question, tag, correct, mask = batch
-    
     
     # change to float
     mask = mask.type(torch.FloatTensor)
@@ -212,8 +205,7 @@ def process_batch(batch, args):
     interaction = interaction.roll(shifts=1, dims=1)
     interaction[:, 0] = 0 # set padding index to the first sequence
     interaction = (interaction * mask).to(torch.int64)
-    # print(interaction)
-    # exit()
+
     #  test_id, question_id, tag
     test = ((test + 1) * mask).to(torch.int64)
     question = ((question + 1) * mask).to(torch.int64)
@@ -224,23 +216,17 @@ def process_batch(batch, args):
     gather_index = torch.tensor(np.count_nonzero(mask, axis=1))
     gather_index = gather_index.view(-1, 1) - 1
 
-
     # device memory로 이동
-
     test = test.to(args.device)
     question = question.to(args.device)
-
-
     tag = tag.to(args.device)
     correct = correct.to(args.device)
     mask = mask.to(args.device)
-
     interaction = interaction.to(args.device)
     gather_index = gather_index.to(args.device)
 
-    return (test, question,
-            tag, correct, mask,
-            interaction, gather_index)
+    return (test, question, tag, correct,
+            mask, interaction, gather_index)
 
 
 # loss계산하고 parameter update!
@@ -253,16 +239,16 @@ def compute_loss(preds, targets):
     """
     loss = get_criterion(preds, targets)
     #마지막 시퀀드에 대한 값만 loss 계산
-    loss = loss[:,-1]
+    loss = loss[:, -1]
     loss = torch.mean(loss)
     return loss
+
 
 def update_params(loss, model, optimizer, args):
     loss.backward()
     torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip_grad)
     optimizer.step()
     optimizer.zero_grad()
-
 
 
 def save_checkpoint(state, model_dir, model_filename):
@@ -272,10 +258,7 @@ def save_checkpoint(state, model_dir, model_filename):
     torch.save(state, os.path.join(model_dir, model_filename))
 
 
-
-def load_model(args):
-    
-    
+def load_model(args):    
     model_path = os.path.join(args.model_dir, args.model_name)
     print("Loading Model from:", model_path)
     load_state = torch.load(model_path)
@@ -283,7 +266,6 @@ def load_model(args):
 
     # 1. load model state
     model.load_state_dict(load_state['state_dict'], strict=True)
-   
-    
     print("Loading Model from:", model_path, "...Finished.")
+
     return model
