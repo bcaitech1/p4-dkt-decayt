d932aeb47c1b12cd21751c62cbb5db67db9ce5b1
ydy8989 <ydy89899@gmail.com>
2021-05-27 17:17:58 +0900

    tensorboard revise, add StepLr and fix seed

--- code/dkt/scheduler.py
+++ code/dkt/scheduler.py
@@ -1,6 +1,5 @@
 
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-
+from torch.optim.lr_scheduler import *
 from transformers import get_linear_schedule_with_warmup
 
 
@@ -11,4 +10,8 @@ def get_scheduler(optimizer, args):
         scheduler = get_linear_schedule_with_warmup(optimizer,
                                                     num_warmup_steps=args.warmup_steps,
                                                     num_training_steps=args.total_steps)
+    elif args.scheduler == 'steplr':
+        #gamma : 한 스텝마다 현재 learning rate에 곱할 값
+        scheduler = StepLR(optimizer, 1, gamma=0.9)  # 794) #gamma : 20epoch => lr x 0.01
+
     return scheduler
